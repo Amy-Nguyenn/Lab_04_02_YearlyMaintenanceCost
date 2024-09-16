15cb438e279e2d0095a3dcaa11c83ef6ce97bf72
Amy <amyphuongnguyenn@gmail.com>
2024-09-16 01:22:13 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+class YearlyMaintenanceCost {
+    public static void main(String[] args) {
+        double winterCost = 453.25;
+        double springCost = 743.42;
+        double fallCost = 583.12;
+        double summerCost =643.23;
+
+        double totalCost = winterCost + springCost + fallCost + summerCost;
+
+        System.out.println("Your Winter Maintenance Cost Is: $" + winterCost);
+        System.out.println("Your Spring Maintenance Cost Is: $" + springCost);
+        System.out.println("Your Fall Maintenance Cost Is: $" + fallCost);
+        System.out.println("Your Summer Maintenance Cost Is: $" + summerCost);
+
+    }
+}
